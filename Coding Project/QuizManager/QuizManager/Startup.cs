ae41e17dae9d00c380bf8348ecf6c2643055f7b6
AoifePearson@DSTOUTPUT.co.uk <AoifePearson@DSTOUTPUT.co.uk>
2019-07-01 11:54:45 +0100

    wireframes, flowchart, project set up

---
+++ Coding Project/QuizManager/QuizManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizManager.Startup))]
+namespace QuizManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
