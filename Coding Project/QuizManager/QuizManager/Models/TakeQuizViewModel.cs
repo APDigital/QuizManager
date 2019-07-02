d9b91f8f6fc1d8b8d02a41a1c6c1b7ec942f34de
AoifePearson@DSTOUTPUT.co.uk <AoifePearson@DSTOUTPUT.co.uk>
2019-07-02 16:57:27 +0100

    cleanup files

--- Coding Project/QuizManager/QuizManager/Models/TakeQuizViewModel.cs
+++ Coding Project/QuizManager/QuizManager/Models/TakeQuizViewModel.cs
@@ -7,16 +7,6 @@ namespace QuizManager.Models
 {
     public class TakeQuizViewModel
     {
-        //TakeQuiz Design:
-        //Get the Full List of Questions Related to a Quiz by the Quiz Title
-        //Add all Questions to AvailableQuestionsByNumber. code will check whether the question has been taken by the Key in the Dictionary.
-        //GetQuestion() will generate a random question from the list of available questions
-        //RemoveQuestionFromAvailableList() will ensure the question cannot be taken a second time
-        //StoreQuestionToUsersAnswers() will keep a record of what answers the user provides
-        //CheckUsersAnswers() will check how many question sthe user answered correctly and set the users final 
-        //score to "Pass" or "Fail" with their pass percentage as a string.
-
-
         private QuizManagerEntities QuizEntities = new QuizManagerEntities();
         public Quiz CurrentQuiz { get; set; }
         public Question CurrentQuestion { get; set; }
